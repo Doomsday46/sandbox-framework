ab4effed81e481987db0240d36ad8ee723ceafa3
Виталий Морозов <v.morozov@it.ecommpay.com>
2020-07-02 10:57:52 +0300

    init

---
+++ Sandbox-framework/Sandbox_framework.h
@@ -0,0 +1,19 @@
+//
+//  Sandbox_framework.h
+//  Sandbox-framework
+//
+//  Created by Виталий Морозов on 30.06.2020.
+//  Copyright © 2020 Виталий Морозов. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for Sandbox_framework.
+FOUNDATION_EXPORT double Sandbox_frameworkVersionNumber;
+
+//! Project version string for Sandbox_framework.
+FOUNDATION_EXPORT const unsigned char Sandbox_frameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Sandbox_framework/PublicHeader.h>
+
+
---
+++ Sandbox-framework/TestClass.swift
@@ -0,0 +1,16 @@
+//
+//  TestClass.swift
+//  Sandbox-framework
+//
+//  Created by Виталий Морозов on 02.07.2020.
+//  Copyright © 2020 Виталий Морозов. All rights reserved.
+//
+
+import Foundation
+
+public class TestClass: NSObject {
+    
+    public static func sum(a: Int, b: Int) -> Int {
+        return a + b
+    }
+}
